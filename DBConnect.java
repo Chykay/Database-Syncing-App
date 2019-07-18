d2e350b238d26316b97fb06944b8e38a97fed4b1
chykay <catueyi@calminfotech.com>
2019-07-18 17:13:13 +0100

    Updated with synclog

--- DBConnect.java
+++ DBConnect.java
@@ -1,14 +1,19 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
 public class DBConnect {
+	private static String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
 
-	public Connection localDB () {
+	public static Connection localDB () throws IOException {
 		// Create a variable for the connection string.
 		Connection con1 = null;
-		String DB = "local/first DB name";
-		String DBUser = "user";
+		String DB = "KokoMD_v2";
+		String DBUser = "sa";
 		String DBPass = "password";
         String connectionUrl = "jdbc:sqlserver://localhost:1433;" + 
         						"databaseName=" + DB + 
@@ -17,30 +22,28 @@ public class DBConnect {
         try {
 			con1 = DriverManager.getConnection(connectionUrl);
 		} catch (SQLException e) {
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
+			List<String> lines = Arrays.asList("Error: Got an exception! '"+ e.getMessage() +"' AT: "+ timeStamp);
+			SyncTables.Slog(lines);
 		} 
-        
         return con1;
 	}
 	
-	public Connection OnlineDB() {
+	public static Connection OnlineDB() throws IOException {
 		// Create a variable for the connection string.
 		Connection con2 = null;
-		String DB = "Online/Second DB name";
-		String DBUser = "user";
-		String DBPass = "password";
-        String connectionUrl = "jdbc:sqlserver://35.256.78.345:1334;" + 
+		String DB = "KokoMD";
+		String DBUser = "koemrusr";
+		String DBPass = "P@ssword247!";
+        String connectionUrl = "jdbc:sqlserver://37.220.93.246:1334;" + 
         						"databaseName=" + DB + 
         						";user=" + DBUser + 
         						";password=" + DBPass ; 
         try {
 			con2 = DriverManager.getConnection(connectionUrl);
 		} catch (SQLException e) {
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
+			List<String> lines = Arrays.asList("Error: Got an exception! '"+ e.getMessage() +"' AT: "+ timeStamp);
+			SyncTables.Slog(lines);
 		} 
-        
         return con2;
 	}
 	
--- SyncTables.java
+++ SyncTables.java
@@ -1,13 +1,21 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class SyncTables {
-	private static ArrayList<String> TablesToBeSynched = new ArrayList<String>();
 	private static ArrayList<String> FFields = new ArrayList<String>(); // ArrayList for foreign fields
 	private static ArrayList<String> RTables = new ArrayList<String>(); // ArrayList for reference tables 
 	private static ArrayList<String> RPFields = new ArrayList<String>(); // ArrayList for reference primary keys
@@ -16,22 +24,33 @@ public class SyncTables {
 	// Hash Map for table columns(key) and data(value) row n, where n = 1,2,3...
 	private static boolean checkFK; // Boolean to hold table foreign key status 
     private static int OrgID = 3; // Company Organisation-ID - used to differentiate company record on the online database
+	private static String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+	public static Path file = Paths.get("SLog.txt"); // Create this file in project directory
     
-	private static Statement localConnection() throws SQLException {
+	private static Statement localConnection() throws SQLException, IOException {
 		// returns local DB connection
-		DBConnect DB = new DBConnect();
-		Connection local = DB.localDB();
+		Connection local = DBConnect.localDB();
 		return local.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 	}
 	
-	private static Statement onlineConnection() throws SQLException{
+	private static Statement onlineConnection() throws SQLException, IOException{
 		// returns online DB connection
-		DBConnect DB = new DBConnect();
-		Connection Online = DB.OnlineDB();
+		Connection Online = DBConnect.OnlineDB();
 		return Online.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 	}
 	
-	private static boolean checkforeignkey(String localtable) throws SQLException {
+	public static void Slog(List<String> lines) throws IOException {
+        Files.write(file, lines, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+	}
+	
+	private static ResultSet getTables() throws SQLException, IOException {
+		// returns foreign key fields in table if any
+		String SQL = "SELECT table_name FROM SyncTables";
+		ResultSet getTables = onlineConnection().executeQuery(SQL);
+		return getTables;
+	}
+	
+	private static boolean checkforeignkey(String localtable) throws SQLException, IOException {
 		// Checks and returns foreign key fields if any
 		FFields.clear();
 		RTables.clear();
@@ -71,21 +90,21 @@ public class SyncTables {
 		return hasFK;
 	}
 
-	private static ResultSet getforeignKeyFields(String table) throws SQLException {
+	private static ResultSet getforeignKeyFields(String table) throws SQLException, IOException {
 		// returns foreign key fields in table if any
 		String SQL = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME='"+ table +"' AND left(CONSTRAINT_NAME,2)='FK'";
 		ResultSet foreignRS = localConnection().executeQuery(SQL);
 		return foreignRS;
 	}
 	
-	private static ResultSet getForeignReferenceTables(String table) throws SQLException {
+	private static ResultSet getForeignReferenceTables(String table) throws SQLException, IOException {
 		// returns reference table(s)
 		String SQL = "SELECT object_name(referenced_object_id) RefTableName FROM sys.foreign_keys WHERE parent_object_id = object_id('"+ table +"')"; 
 		ResultSet FRefRS = onlineConnection().executeQuery(SQL);
 		return FRefRS;
 	}
 	
-	private static ResultSet getRefPrimaryKey(String table) throws SQLException {
+	private static ResultSet getRefPrimaryKey(String table) throws SQLException, IOException {
 		// returns reference primary keys
 		String SQL = "SELECT column_name FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE OBJECTPROPERTY(OBJECT_ID(constraint_name), 'IsPrimaryKey') = 1"
 				+ " AND table_name = '" + table + "'";
@@ -93,7 +112,7 @@ public class SyncTables {
 		return RefPK;
 	}
 	
-	private static ResultSet getLocalPrimaryKey(String table) throws SQLException {
+	private static ResultSet getLocalPrimaryKey(String table) throws SQLException, IOException {
 		// returns primary key name from table
 		String SQL = "SELECT column_name FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE OBJECTPROPERTY(OBJECT_ID(constraint_name), 'IsPrimaryKey') = 1"
 				+ " AND table_name = '" + table + "'";
@@ -101,21 +120,21 @@ public class SyncTables {
 		return RefPK;
 	}
 
-	private static ResultSet getLocalTableData(String table) throws SQLException {
+	private static ResultSet getLocalTableData(String table) throws SQLException, IOException {
 		// returns data ResultSet from table
 		String SQL = "SELECT * FROM [dbo]."+table+" WHERE [synched] = 0";
 		ResultSet localRS = localConnection().executeQuery(SQL);
 		return localRS;
 	}
 	
-	private static ResultSet getNewRefPKey(String table, String PKey, String FLocalID) throws SQLException {
+	private static ResultSet getNewRefPKey(String table, String PKey, String FLocalID) throws SQLException, IOException {
 		// returns primary key from table using local ID
 		String SQL = "SELECT "+ PKey +" FROM "+ table +" WHERE localID = "+FLocalID;
 		ResultSet RefPK = onlineConnection().executeQuery(SQL);
 		return RefPK;
 	}
 	
-	private static boolean checkOnlineTable(String table, String localID) throws SQLException {
+	private static boolean checkOnlineTable(String table, String localID) throws SQLException, IOException {
 		// checks online table if record exists and returns boolean
 		if(localID == "") {
 			return false;
@@ -130,14 +149,14 @@ public class SyncTables {
 		}
 	}
 	
-	private static ResultSet checkreference(String table, String PKey) throws SQLException {
+	private static ResultSet checkreference(String table, String PKey) throws SQLException, IOException {
 		// gets online reference table record
 		String SQL = "SELECT localID FROM "+ table +" WHERE localID = "+ PKey +" AND OrgID = "+OrgID;
 		ResultSet checker = onlineConnection().executeQuery(SQL);
 		return checker;
 	}
 	
-	private static int synchOnlineInsert(String table, String localID) throws SQLException {
+	private static int synchOnlineInsert(String table, String localID) throws SQLException, IOException {
 		// sync table by table insert
 		String column = "";
 		String value = "";
@@ -152,7 +171,7 @@ public class SyncTables {
 		return RefPK;
 	}
 	
-	private static int synchOnlineUpdate(String table, String localID) throws SQLException {
+	private static int synchOnlineUpdate(String table, String localID) throws SQLException, IOException {
 		// sync table by table update
 		String update = "";
 		for (String i : tabledata.keySet()) {
@@ -164,107 +183,132 @@ public class SyncTables {
 		return RefPK;
 	}
 	
-	public static int setsynchstatus(String table, String PKField, String PKey) throws SQLException {
+	public static int setsynchstatus(String table, String PKField, String PKey) throws SQLException, IOException {
 		// update synch status locally
 		String SQL = "Update [dbo].["+ table +"] Set [Synched] = 1 where "+ PKField +" = "+PKey;
 		int RefPK = localConnection().executeUpdate(SQL);
 		return RefPK;
 	}
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		String localtable;
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        TablesToBeSynched.add("tablename"); // add table to be synched to array 'TablesToBeSynched'
-        for (int z = 0; z < TablesToBeSynched.size(); z++) {
-        	// loops through tables to be synched
-	        localtable = TablesToBeSynched.get(z);
-	        checkFK = checkforeignkey(localtable); // checks if table has foreign key
-			String PKeyField = null;
-			ResultSet getlocaldata = getLocalTableData(localtable); // gets table data
-			ResultSetMetaData metaData3 = getlocaldata.getMetaData();
-			int count3 = metaData3.getColumnCount();
-			while(getlocaldata.next()) {
-				tabledata.clear();
-				RefCheck.clear();
-				for (int k = 1; k <= count3; k++) {
-					tabledata.put(metaData3.getColumnName(k), getlocaldata.getString(k)); // adds table data to HashMap
-				}
-				String PKeyF = "";
-				ResultSet CheckPKey = getLocalPrimaryKey(localtable); // check if table has primary key(which is assumed to be auto increment)
-				while(CheckPKey.next()) {
-					PKeyF = CheckPKey.getString("column_name");
-				}
-				if(PKeyF != "") {
-					tabledata.remove(PKeyF); // removes primary key from HashMap if available
-				}
-				if(checkFK == true) {
-					// perform the following if the table has foreign key(s)
-					for (int i = 0; i < FFields.size(); i++) {
-						// gets the new online primary key for current record in loop
-						ResultSet onlinePKeyFields = getNewRefPKey(RTables.get(i), RPFields.get(i), getlocaldata.getString(FFields.get(i)));
-						while(onlinePKeyFields.next()) {
-							// replace reference primary key in foreign key field(s)
-							tabledata.replace(FFields.get(i), onlinePKeyFields.getString(RPFields.get(i)));
-						}
-						// checks if record exist on reference table and store in array
-						ResultSet RecordCheck = checkreference(RTables.get(i), getlocaldata.getString(FFields.get(i)));
-						while(RecordCheck.next()) {
-							// add record to array
-							RefCheck.add(RecordCheck.getString("localID"));
+	public static void main(String[] args) throws IOException {
+		try {
+			String localtable;
+	        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	        ResultSet getTables = getTables();
+	        
+	        List<String> empty = Arrays.asList("----- Start Program log");
+	        Files.write(file, empty, Charset.forName("UTF-8")); // should clear the log file
+	        
+	        while (getTables.next()) {
+	        	// loops through tables to be synced
+		        localtable = getTables.getString("table_name");
+		        List<String> lines = Arrays.asList("Syncing Table '"+ localtable +"' AT: "+ timeStamp);
+		        Slog(lines);
+	        
+		        checkFK = checkforeignkey(localtable); // checks if table has foreign key
+				String PKeyField = null;
+				ResultSet getlocaldata = getLocalTableData(localtable); // gets table data
+				ResultSetMetaData metaData3 = getlocaldata.getMetaData();
+				int count3 = metaData3.getColumnCount();
+				while(getlocaldata.next()) {
+					tabledata.clear();
+					RefCheck.clear();
+					for (int k = 1; k <= count3; k++) {
+						tabledata.put(metaData3.getColumnName(k), getlocaldata.getString(k)); // adds table data to HashMap
+					}
+					String PKeyF = "";
+					ResultSet CheckPKey = getLocalPrimaryKey(localtable); // check if table has primary key(which is assumed to be auto increment)
+					while(CheckPKey.next()) {
+						PKeyF = CheckPKey.getString("column_name");
+					}
+					if(PKeyF != "") {
+						tabledata.remove(PKeyF); // removes primary key from HashMap if available
+					}
+					if(checkFK == true) {
+						// perform the following if the table has foreign key(s)
+						for (int i = 0; i < FFields.size(); i++) {
+							// gets the new online primary key for current record in loop
+							ResultSet onlinePKeyFields = getNewRefPKey(RTables.get(i), RPFields.get(i), getlocaldata.getString(FFields.get(i)));
+							while(onlinePKeyFields.next()) {
+								// replace reference primary key in foreign key field(s)
+								tabledata.replace(FFields.get(i), onlinePKeyFields.getString(RPFields.get(i)));
+							}
+							// checks if record exist on reference table and store in array
+							ResultSet RecordCheck = checkreference(RTables.get(i), getlocaldata.getString(FFields.get(i)));
+							while(RecordCheck.next()) {
+								// add record to array
+								RefCheck.add(RecordCheck.getString("localID"));
+							}
 						}
 					}
-				}
-				
-				if(checkFK == true) {
-					if(FFields.size() != RefCheck.size()){
-						// basically checks if the size of the foreign keys arrays is same as the data gotten from their reference table(s) array 
-						System.out.println("we got here because some foreign key dependency data is not available :(");
-						System.out.println(" -------- We are DONE. THE EnD! -------- ");
-						break;
-					}
-				}
-				String PKey = "";
-				boolean check;
-				ResultSet ChPKey = getLocalPrimaryKey(localtable); // gets local primary key
-					ResultSet getRPKey = getLocalPrimaryKey(localtable);
-					if(ChPKey.isBeforeFirst()) {
-						while(getRPKey.next()) {
-							PKeyField = getRPKey.getString("column_name");
+					
+					if(checkFK == true) {
+						if(FFields.size() != RefCheck.size()){
+							// basically checks if the size of the foreign keys arrays is same as the data gotten from their reference table(s) array 
+							List<String> error = Arrays.asList("--Error: Foreign key dependency data not available :( ☹  for (foreign)table '"+ localtable +"' AT: "+ timeStamp);
+					        Slog(error);
+							break;
 						}
-						PKey = getlocaldata.getString(PKeyField);
-						check = checkOnlineTable(localtable, PKey); //checks if record exist in table using Local ID
-					}else {
-						check = false;
 					}
-
-					// commence synching
-					if(check) {
-						//updates record if exists
-						int update = synchOnlineUpdate(localtable,PKey);
-						if(update == 1) {
-							System.out.println("Synch: Update Successful");
-							int synched = setsynchstatus(localtable,PKeyField,PKey); //set status to synched in local table
-							if(synched == 1) {
-								System.out.println("Synch Down Successful");
+					String PKey = "";
+					boolean check;
+					ResultSet ChPKey = getLocalPrimaryKey(localtable); // gets local primary key
+						ResultSet getRPKey = getLocalPrimaryKey(localtable);
+						if(ChPKey.isBeforeFirst()) {
+							while(getRPKey.next()) {
+								PKeyField = getRPKey.getString("column_name");
+							}
+							PKey = getlocaldata.getString(PKeyField);
+							check = checkOnlineTable(localtable, PKey); //checks if record exist in table using Local ID
+						}else {
+							check = false;
+						}
+	
+						// commence syncing
+						if(check) {
+							//updates record if exists
+							int update = synchOnlineUpdate(localtable,PKey);
+							if(update == 1) {
+								List<String> note = Arrays.asList("Online Sync Update Successful for table '"+ localtable +"' AT: "+ timeStamp);
+						        Slog(note);
+								int synched = setsynchstatus(localtable,PKeyField,PKey); //set status to synched in local table
+								if(synched == 1) {
+									List<String> Stat = Arrays.asList("Local Sync Status updated for table '"+ localtable +"' AT: "+ timeStamp);
+							        Slog(Stat);
+								}
+							}else {
+								List<String> note = Arrays.asList("Online Sync Update Error for table '"+ localtable +"' AT: "+ timeStamp);
+						        Slog(note);
 							}
 						}else {
-							System.err.println("Synch: Update NOT Successful");
-						}
-					}else {
-						// insert record if it does not exist
-						int insert = synchOnlineInsert(localtable,PKey);
-						if(insert == 1) {
-							System.out.println("Synch: Insert Successful");
-							int synched = setsynchstatus(localtable,PKeyField,PKey); //set status to synched in local table
-							if(synched == 1) {
-								System.out.println("Synch Down Successful");
+							// insert record if it does not exist
+							int insert = synchOnlineInsert(localtable,PKey);
+							if(insert == 1) {
+								List<String> note = Arrays.asList("Online Sync Insert Successful for table '"+ localtable +"' AT: "+ timeStamp);
+						        Slog(note);
+								int synched = setsynchstatus(localtable,PKeyField,PKey); //set status to synched in local table
+								if(synched == 1) {
+									List<String> Stat = Arrays.asList("Local Sync Status updated for table '"+ localtable +"' AT: "+ timeStamp);
+							        Slog(Stat);
+								}
+							}else {
+								List<String> note = Arrays.asList("Online Sync Insert Error for table '"+ localtable +"' AT: "+ timeStamp);
+						        Slog(note);
 							}
-						}else {
-							System.err.println("Synch: Insert NOT Successful");
 						}
-					}
-			}
-        }
-        System.out.println(" -------- We are DONE. THE EnD! -------- ");
+				}
+	        }
+		}catch(ClassNotFoundException e) {
+			List<String> note = Arrays.asList("Error: "+e+" At "+ timeStamp);
+	        Slog(note);
+		}catch(SQLException e) {
+			List<String> note = Arrays.asList("Error: "+e+" At "+ timeStamp);
+	        Slog(note);
+		}catch(IOException e) {
+			List<String> note = Arrays.asList("Error: "+e+" At "+ timeStamp);
+	        Slog(note);
+		}
+        List<String> note = Arrays.asList("----- End: Program Stopped AT "+ timeStamp + System.lineSeparator());
+        Slog(note);
 	}
 }
